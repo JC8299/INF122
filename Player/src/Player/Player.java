64365e2687bd5471134c7831afa569b1f0c06060
JC8299 <32472671+JC8299@users.noreply.github.com>
2021-03-09 23:41:40 -0800

    Update Player.java

--- Player/src/Player/Player.java
+++ Player/src/Player/Player.java
@@ -1,11 +1,32 @@
 package Player;
 
 public class Player {
-    public int score1;
-    public int score2;
-    public Player(){
+    public string name;
+    public int scoreOne;
+    public int scoreTwo;
+    public Player(string n){
+        name = n;
         score1 = 0;
         score2 = 0;
     }
 
+    public string getName() {
+        return name;
+    }
+    
+    public void setScoreOne(int s) {
+        scoreOne = s;
+    }
+    
+    public int getScoreOne() {
+        return scoreOne;
+    }
+    
+    public int setScoreTwo(int s) {
+        scoreTwo = s;
+    }
+    
+    public int getScoreTwo() {
+        return scoreTwo;
+    }
 }
